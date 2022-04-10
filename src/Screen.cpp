0ff30e8ab60e0567861f803a2deacafa20fec3cb
zjcrumpton <zjcrumpton@gmail.com>
2022-04-10 15:20:12 -0600

    hide cursor

--- src/Screen.cpp
+++ src/Screen.cpp
@@ -9,6 +9,7 @@ Screen::Screen() {
          // the program
   keypad(stdscr, TRUE); // get access to F1, F2 etc
   noecho(); // don't echo typed user input characters when calling getch() etc
+  curs_set(0); // hide the cursor
   initColors();
 };
 
